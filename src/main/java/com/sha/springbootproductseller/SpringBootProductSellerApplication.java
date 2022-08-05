83188d6e222a27b359176619bb4d20de1c735ee9
심태진 <simtaejin@simtaejin-ui-MacBookPro.local>
2022-08-05 17:23:33 +0900

    first commit

---
+++ src/main/java/com/sha/springbootproductseller/SpringBootProductSellerApplication.java
@@ -0,0 +1,13 @@
+package com.sha.springbootproductseller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProductSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProductSellerApplication.class, args);
+	}
+
+}
